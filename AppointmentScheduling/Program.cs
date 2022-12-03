3e99dd077032bad775dbe8ce03341c28f85964a0
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-12-03 06:54:19 +0200

    Identity init

--- AppointmentScheduling/Program.cs
+++ AppointmentScheduling/Program.cs
@@ -1,15 +1,19 @@
+using AppointmentScheduling.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var config = builder.Configuration;
+
+builder.Services.AddDbContext<AppDbContext>(o => 
+    o.UseSqlServer(config.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
